1db9bc4f9c50dce96629db05148c7ae99f59b482
akira-cn <akira.cn@gmail.com>
2018-06-12 23:27:08 +0800

    update

--- src/render.js
+++ src/render.js
@@ -12,14 +12,14 @@ export default class extends CustomRenderer {
       this.element.parent.resolution = [width, height]
     }
   }
-  onParticleCreated(particle) {
+  createParticle(particle) {
     const s = new Sprite()
     if(particle.body) {
       s.textures = particle.body
     } else {
       const r = particle.radius
       s.attr({
-        bgcolor: `#${particle.color || 'FF0000'}`,
+        bgcolor: particle.color || '#FF0000',
         size: [r * 2, r * 2],
         borderRadius: r,
       })
@@ -32,11 +32,16 @@ export default class extends CustomRenderer {
       scale: [particle.scale, particle.scale],
       rotate: particle.rotation,
     })
+    return s
+  }
+  onParticleCreated(particle) {
+    const s = this.createParticle(particle)
     particle.body = s
     this.element.append(s)
   }
   onParticleUpdate(particle) {
     const s = particle.body
+
     if(particle.transform.rgb && (!s.textures.length)) {
       const {r, g, b} = particle.transform.rgb
       const bgcolor = `rgba(${r}, ${g}, ${b}, ${particle.alpha})`
@@ -44,7 +49,6 @@ export default class extends CustomRenderer {
     } else {
       s.attr({opacity: particle.alpha})
     }
-
     s.attr({
       pos: [particle.p.x, particle.p.y],
       scale: [particle.scale, particle.scale],
@@ -52,7 +56,8 @@ export default class extends CustomRenderer {
     })
   }
   onParticleDead(particle) {
-    this.element.remove(particle.body)
+    const s = particle.body
+    this.element.remove(s)
     particle.body = null
   }
 }
